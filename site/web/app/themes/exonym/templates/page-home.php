f8f38384c08a84013610d7afa24047bee1d16edc
Dustin Jones <info@domesticjones.com>
2018-11-12 14:56:01 -0700

    Create background module, create basic home page fields

--- site/web/app/themes/exonym/templates/page-home.php
+++ site/web/app/themes/exonym/templates/page-home.php
@@ -1,10 +1,17 @@
 <?php
   /*
-  ===============================
-    [[[  TEMPLATE NAME: Home  ]]]
-  ===============================
+  ===================
+  TEMPLATE NAME: Home
+  ===================
   */
   get_header();
+  if(have_posts()): while(have_posts()): the_post();
+  get_template_part('templates/background');
 ?>
-
-<?php get_footer(); ?>
+<section class="page-section">
+  <img src="<?php ex_logo('primary', 'light'); ?>" alt="Logo for <?php ex_brand(); ?>" class="logo-home" />
+</section>
+<?php
+  endwhile; endif;
+  get_footer();
+?>
